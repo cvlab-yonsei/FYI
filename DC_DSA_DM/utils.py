41fa3672ca42415a26a4b5f24fc8df7008c15857
sonbk220 <sonbk220@yonsei.ac.kr>
2024-08-18 06:31:06 +0000

    fix dsa

--- DC_DSA_DM/utils.py
+++ DC_DSA_DM/utils.py
@@ -618,7 +618,7 @@ def rand_scale(x, param):
             [0,  sy[i], 0],] for i in range(x.shape[0])]
     theta = torch.tensor(theta, dtype=torch.float)
     if param.Siamese: # Siamese augmentation:
-        theta[:] = theta[0]
+        theta[:] = theta[0].clone()
     grid = F.affine_grid(theta, x.shape, align_corners=True).to(x.device)
     x = F.grid_sample(x, grid, align_corners=True)
     return x
@@ -632,7 +632,7 @@ def rand_rotate(x, param): # [-180, 180], 90: anticlockwise 90 degree
         [torch.sin(theta[i]), torch.cos(theta[i]),  0],]  for i in range(x.shape[0])]
     theta = torch.tensor(theta, dtype=torch.float)
     if param.Siamese: # Siamese augmentation:
-        theta[:] = theta[0]
+        theta[:] = theta[0].clone()
     grid = F.affine_grid(theta, x.shape, align_corners=True).to(x.device)
     x = F.grid_sample(x, grid, align_corners=True)
     return x
@@ -643,7 +643,7 @@ def rand_flip(x, param):
     set_seed_DiffAug(param)
     randf = torch.rand(x.size(0), 1, 1, 1, device=x.device)
     if param.Siamese: # Siamese augmentation:
-        randf[:] = randf[0]
+        randf[:] = randf[0].clone()
     return torch.where(randf < prob, x.flip(3), x)
 
 
@@ -652,7 +652,7 @@ def rand_brightness(x, param):
     set_seed_DiffAug(param)
     randb = torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device)
     if param.Siamese:  # Siamese augmentation:
-        randb[:] = randb[0]
+        randb[:] = randb[0].clone()
     x = x + (randb - 0.5)*ratio
     return x
 
@@ -663,7 +663,7 @@ def rand_saturation(x, param):
     set_seed_DiffAug(param)
     rands = torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device)
     if param.Siamese:  # Siamese augmentation:
-        rands[:] = rands[0]
+        rands[:] = rands[0].clone()
     x = (x - x_mean) * (rands * ratio) + x_mean
     return x
 
@@ -674,7 +674,7 @@ def rand_contrast(x, param):
     set_seed_DiffAug(param)
     randc = torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device)
     if param.Siamese:  # Siamese augmentation:
-        randc[:] = randc[0]
+        randc[:] = randc[0].clone()
     x = (x - x_mean) * (randc + ratio) + x_mean
     return x
 
@@ -688,8 +688,8 @@ def rand_crop(x, param):
     set_seed_DiffAug(param)
     translation_y = torch.randint(-shift_y, shift_y + 1, size=[x.size(0), 1, 1], device=x.device)
     if param.Siamese:  # Siamese augmentation:
-        translation_x[:] = translation_x[0]
-        translation_y[:] = translation_y[0]
+        translation_x[:] = translation_x[0].clone()
+        translation_y[:] = translation_y[0].clone()
     grid_batch, grid_x, grid_y = torch.meshgrid(
         torch.arange(x.size(0), dtype=torch.long, device=x.device),
         torch.arange(x.size(2), dtype=torch.long, device=x.device),
@@ -710,8 +710,8 @@ def rand_cutout(x, param):
     set_seed_DiffAug(param)
     offset_y = torch.randint(0, x.size(3) + (1 - cutout_size[1] % 2), size=[x.size(0), 1, 1], device=x.device)
     if param.Siamese:  # Siamese augmentation:
-        offset_x[:] = offset_x[0]
-        offset_y[:] = offset_y[0]
+        offset_x[:] = offset_x[0].clone()
+        offset_y[:] = offset_y[0].clone()
     grid_batch, grid_x, grid_y = torch.meshgrid(
         torch.arange(x.size(0), dtype=torch.long, device=x.device),
         torch.arange(cutout_size[0], dtype=torch.long, device=x.device),
--- MTT/utils.py
+++ MTT/utils.py
@@ -612,7 +612,7 @@ def rand_scale(x, param):
             [0,  sy[i], 0],] for i in range(x.shape[0])]
     theta = torch.tensor(theta, dtype=torch.float)
     if param.batchmode: # batch-wise:
-        theta[:] = theta[0]
+        theta[:] = theta[0].clone()
     grid = F.affine_grid(theta, x.shape, align_corners=True).to(x.device)
     x = F.grid_sample(x, grid, align_corners=True)
     return x
@@ -626,7 +626,7 @@ def rand_rotate(x, param): # [-180, 180], 90: anticlockwise 90 degree
         [torch.sin(theta[i]), torch.cos(theta[i]),  0],]  for i in range(x.shape[0])]
     theta = torch.tensor(theta, dtype=torch.float)
     if param.batchmode: # batch-wise:
-        theta[:] = theta[0]
+        theta[:] = theta[0].clone()
     grid = F.affine_grid(theta, x.shape, align_corners=True).to(x.device)
     x = F.grid_sample(x, grid, align_corners=True)
     return x
@@ -637,7 +637,7 @@ def rand_flip(x, param):
     set_seed_DiffAug(param)
     randf = torch.rand(x.size(0), 1, 1, 1, device=x.device)
     if param.batchmode: # batch-wise:
-        randf[:] = randf[0]
+        randf[:] = randf[0].clone()
     return torch.where(randf < prob, x.flip(3), x)
 
 
@@ -646,7 +646,7 @@ def rand_brightness(x, param):
     set_seed_DiffAug(param)
     randb = torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device)
     if param.batchmode:  # batch-wise:
-        randb[:] = randb[0]
+        randb[:] = randb[0].clone()
     x = x + (randb - 0.5)*ratio
     return x
 
@@ -657,7 +657,7 @@ def rand_saturation(x, param):
     set_seed_DiffAug(param)
     rands = torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device)
     if param.batchmode:  # batch-wise:
-        rands[:] = rands[0]
+        rands[:] = rands[0].clone()
     x = (x - x_mean) * (rands * ratio) + x_mean
     return x
 
@@ -668,7 +668,7 @@ def rand_contrast(x, param):
     set_seed_DiffAug(param)
     randc = torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device)
     if param.batchmode:  # batch-wise:
-        randc[:] = randc[0]
+        randc[:] = randc[0].clone()
     x = (x - x_mean) * (randc + ratio) + x_mean
     return x
 
@@ -682,8 +682,8 @@ def rand_crop(x, param):
     set_seed_DiffAug(param)
     translation_y = torch.randint(-shift_y, shift_y + 1, size=[x.size(0), 1, 1], device=x.device)
     if param.batchmode:  # batch-wise:
-        translation_x[:] = translation_x[0]
-        translation_y[:] = translation_y[0]
+        translation_x[:] = translation_x[0].clone()
+        translation_y[:] = translation_y[0].clone()
     grid_batch, grid_x, grid_y = torch.meshgrid(
         torch.arange(x.size(0), dtype=torch.long, device=x.device),
         torch.arange(x.size(2), dtype=torch.long, device=x.device),
@@ -704,8 +704,8 @@ def rand_cutout(x, param):
     set_seed_DiffAug(param)
     offset_y = torch.randint(0, x.size(3) + (1 - cutout_size[1] % 2), size=[x.size(0), 1, 1], device=x.device)
     if param.batchmode:  # batch-wise:
-        offset_x[:] = offset_x[0]
-        offset_y[:] = offset_y[0]
+        offset_x[:] = offset_x[0].clone()
+        offset_y[:] = offset_y[0].clone()
     grid_batch, grid_x, grid_y = torch.meshgrid(
         torch.arange(x.size(0), dtype=torch.long, device=x.device),
         torch.arange(cutout_size[0], dtype=torch.long, device=x.device),
