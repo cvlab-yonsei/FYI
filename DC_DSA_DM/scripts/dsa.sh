12294b21b588dfd3a7ff5e4ccf2940a39b9c7036
sonbk220 <sonbk220@yonsei.ac.kr>
2024-08-30 05:53:28 +0000

    revise

--- DC_DSA_DM/scripts/dsa.sh
+++ DC_DSA_DM/scripts/dsa.sh
@@ -3,5 +3,6 @@ IPC=10
 NETWORK="ConvNet"
 RUNNAME="DSAFYI"
 TAGS="CIFAR10_10IPC"
+DEVICE='0'
 
-python main.py  --dataset ${DATASET}  --model ${NETWORK}  --ipc ${IPC}  --init real  --method DSA  --dsa_strategy color_crop_cutout_flip_scale_rotate --run_name ${RUNNAME} --run_tags ${TAGS}
+python main.py  --dataset ${DATASET}  --model ${NETWORK}  --ipc ${IPC}  --init real  --method DSA  --dsa_strategy color_crop_cutout_flip_scale_rotate --run_name ${RUNNAME} --run_tags ${TAGS} --device ${DEVICE}
